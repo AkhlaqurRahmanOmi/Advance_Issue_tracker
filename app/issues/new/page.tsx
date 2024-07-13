78f5dd72a61ea0d93b4849220a33532476edeca0
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmail.com>
2024-07-14 00:33:16 +0600

    handle errrors and showing

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
 import { useForm, Controller } from 'react-hook-form'
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 // import SimpleMDE from "react-simplemde-editor";
@@ -16,17 +16,26 @@ interface IssueForm {
 const NewIssuePage = () => {
     const router = useRouter()
     const { register, control, handleSubmit } = useForm<IssueForm>();
+    const [error, setError] = useState('');
     return (
-        <form className='max-w-xl space-y-5' onSubmit={handleSubmit(async (data) => {
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-        })}>
-            <TextField.Root placeholder='title' {...register('title')} />
-            {/* <Controller name='description' control={control} render={({ field }) => <TextArea placeholder='issues' />} /> */}
-            <TextArea placeholder='issues' {...register('description')} />
-            <Button>Submit New Issue</Button>
-
-        </form>
+        <div className='max-w-xl'>
+            {error && <Callout.Root color='red' className='mb-6'>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+            <form className=' space-y-5' onSubmit={handleSubmit(async (data) => {
+                try {
+                    await axios.post('/api/issues', data)
+                    router.push('/issues')
+                } catch (err) {
+                    setError('An unexpected error occurred')
+                }
+            })}>
+                <TextField.Root placeholder='title' {...register('title')} />
+                {/* <Controller name='description' control={control} render={({ field }) => <TextArea placeholder='issues' />} /> */}
+                <TextArea placeholder='issues' {...register('description')} />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     )
 }
 
